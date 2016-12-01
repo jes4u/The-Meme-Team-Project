055931238a33536224d0a7990bf0c2fde0d4d6db
Ryan Zuzelski <rzuzelski@gmail.com>
2016-12-01 15:15:18 -0800

    Authenticated, can't find response with auth token

--- scripts/test.r
+++ scripts/test.r
@@ -1,15 +1,27 @@
 #Created by Jesse:
-
 library(jsonlite)
 library(dplyr)
+
 library(shiny)
 library(dplyr)
-library(Rfacebook)
-
+library(httr)
 # Auth. Key
 app_id <- "1850413891841129"
 
-fbOAuth(app_id, app_secret, extended_permissions = FALSE,
-        legacy_permissions = FALSE)
+#AMS auth
+customer_id <- '2557'
+api_key <- 'hb2r82i8saloj1ectsfsi5omlq'
+
+base <- "http://api-v2.applymagicsauce.com/"
+auth_req <- paste0(base, "auth")
+auth_data <- POST(url=auth_req, 
+                  body=list(
+                    "customer_id"='2557',
+                    "api_key"='hb2r82i8saloj1ectsfsi5omlq'
+                  ),
+                  encode="json",
+                  content_type_json(),
+                  accept_json()
+             )
 
 
