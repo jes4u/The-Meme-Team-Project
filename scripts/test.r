1bf0ad031977cabe64f34738510604a25baad5d2
Ryan Zuzelski <rzuzelski@gmail.com>
2016-12-02 11:25:22 -0800

    add token, request

--- scripts/test.r
+++ scripts/test.r
@@ -11,17 +11,29 @@ app_id <- "1850413891841129"
 #AMS auth
 customer_id <- '2557'
 api_key <- 'hb2r82i8saloj1ectsfsi5omlq'
+need_new <- 0
 
 base <- "http://api-v2.applymagicsauce.com/"
-auth_req <- paste0(base, "auth")
-auth_data <- POST(url=auth_req, 
-                  body=list(
-                    "customer_id"='2557',
-                    "api_key"='hb2r82i8saloj1ectsfsi5omlq'
-                  ),
-                  encode="json",
-                  content_type_json(),
-                  accept_json()
-             )
+if (need_new > 0)  {
+  r <- POST(paste0(base, "auth"), 
+            add_headers("Content-Type"="application/json"), 
+            body = '{"customer_id": 2557, 
+                    "api_key": "hb2r82i8saloj1ectsfsi5omlq"}')
+  
+            if (r$status_code != 200) {
+              print("Bad key/id")
+            } else {
+              auth_response <- content(r)
+            }
+}
 
+token <- auth_response$token
+
+ams_req <- POST(paste0(base, "text?", "source=TWEET", "&interpretations=true"),
+                add_headers("X-Auth-Token"=token,
+                            "Content-type"="application/json"),
+                body='ISIS is taking credit for the terrible stabbing attack 
+                      at Ohio State University by a Somali refugee who should 
+                      not have been in our country.')
+ams_resp <- content(ams_req)
 
