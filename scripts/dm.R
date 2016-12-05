5884efc22a6eadffddba2e8eb8d6329644b1629c
joshuaji03 <yixuaj@uw.edu>
2016-12-05 14:24:10 -0800

    Update dm.R

--- scripts/dm.R
+++ scripts/dm.R
@@ -3,6 +3,8 @@
 library(dplry)
 
 dm <- function(ams_data_frame1) {
+  max <- rep(1, length(1:5))
+  min <- rep(0, length(1:5))
   colnames(ams_data_frame1)[7] <- "age"
   colnames(ams_data_frame1)[9] <- "gender"
   colnames(ams_data_frame1)[8] <- "age_num"
@@ -17,7 +19,14 @@ dm <- function(ams_data_frame1) {
   rownames(traits) <- c()
   colnames(traits) <- c("trait")
   data_table <- cbind(traits, value)
-  return(data_table)
+  data_table2 <- data.frame(matrix(seq(5),nrow = 1, ncol = 5))
+  data_table2 <- rbind(data_table2, max, min, data_table[,2])
+  colnames(data_table2) <- data_table[,1]
+  data_table2 <- data_table2[-c(1),]
+  rownames(data_table2)[1] <- "max"
+  rownames(data_table2)[2] <- "min"
+  rownames(data_table2)[3] <- "value"
+  return(data_table2)
 }
 
 
