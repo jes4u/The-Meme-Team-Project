740d57a7ba8fbb61ec726f4878da4f475988f13e
joshuaji03 <yixuaj@uw.edu>
2016-12-06 14:44:46 -0800

    Update dm.R

--- scripts/dm.R
+++ scripts/dm.R
@@ -4,26 +4,30 @@ library(dplyr)
 
 GetBig5DF <- function(pred_df) {
   df <- data.frame(matrix(nrow = 1, ncol = 5))
+  pred_df <- pred_df %>% select(-contains("3"), -contains("4"))
+  data <- pred_df %>% select(contains("value"))
+  df[1,] <- data
+  df <- rbind(rep(1,5) , rep(0,5) , data)
   colnames(df) <- c("Openness",
                     "Extraversion",
                     "Agreeableness",
                     "Neuroticism",
                     "Conscientiousness")
-  pred_df <- pred_df %>% select(-contains("3"), -contains("4"))
-  data <- pred_df %>% select(contains("value"))
-  df[1,] <- data
-  df <- rbind(rep(5,5) , rep(0,5) , data)
   return(df)
 }
+
 
 #Creates other df from interpretations df and 2 elements of predictions df
 
 GetOtherDF <- function(pred_df, int_df) {
-  df <- data.frame(matrix(nrow = 1, ncol = 4))
-  colnames(df) <- c("Age",
-                    "Female",
-                    "Leadership",
-                    "Jung_Type")
-  pred_df <- pred_df %>% select(contains("value.3"), contains("value.4"))
-  ###mutate int_df columns onto pred_df, df[1,] <- data###
+  df_1 <- data.frame(matrix(nrow = 3, ncol = 2))
+  trait <- c("Female","Age","Jungian_Personality", "Leadership" )
+  pred_df <- pred_df %>% select(contains("value.3"), contains("value.4")) %>%
+    t()
+  int_df <- int_df %>% select(contains("value"), contains("value.1")) %>%
+    t()
+  value <- rbind(pred_df, int_df)
+  df_1 <- data.frame(trait, value)
+  rownames(df_1) <- c(1:4)
+  return(df_1)
 }
