5a58c053b38c2e5111672601166076568642eb8d
joshuaji03 <yixuaj@uw.edu>
2016-12-06 14:45:50 -0800

    Update dm.R

--- scripts/dm.R
+++ scripts/dm.R
@@ -20,7 +20,7 @@ GetBig5DF <- function(pred_df) {
 #Creates other df from interpretations df and 2 elements of predictions df
 
 GetOtherDF <- function(pred_df, int_df) {
-  df_1 <- data.frame(matrix(nrow = 3, ncol = 2))
+  df_1 <- data.frame(matrix(nrow = 0, ncol = 2))
   trait <- c("Female","Age","Jungian_Personality", "Leadership" )
   pred_df <- pred_df %>% select(contains("value.3"), contains("value.4")) %>%
     t()
