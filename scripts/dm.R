65ffb8895cb2f25a3f7650b71818c01bfd76508b
Ryan Zuzelski <rzuzelski@gmail.com>
2016-12-05 16:03:01 -0800

    4:02

--- scripts/dm.R
+++ scripts/dm.R
@@ -1,39 +1,42 @@
 ## setwed()
 ## source()
-library(dplry)
+library(dplyr)
+# 
+# dm <- function(ams_data_frame1) {
+#   max <- rep(1, length(1:5))
+#   min <- rep(0, length(1:5))
+#   colnames(ams_data_frame1)[7] <- "age"
+#   colnames(ams_data_frame1)[9] <- "gender"
+#   colnames(ams_data_frame1)[8] <- "age_num"
+#   colnames(ams_data_frame1)[10] <- "gender_prob"
+#   value <- select(ams_data_frame1, contains("value")) %>%
+#   t()
+#   rownames(value) <- c()
+#   colnames(value) <- c("value")
+#   
+#   traits <- select(ams_data_frame1, contains("trait")) %>%
+#     t() 
+#   rownames(traits) <- c()
+#   colnames(traits) <- c("trait")
+#   data_table <- cbind(traits, value)
+#   data_table2 <- data.frame(matrix(seq(5),nrow = 1, ncol = 5))
+#   data_table2 <- rbind(data_table2, max, min, data_table[,2])
+#   colnames(data_table2) <- data_table[,1]
+#   data_table2 <- data_table2[-c(1),]
+#   rownames(data_table2)[1] <- 1
+#   rownames(data_table2)[2] <- 2
+#   rownames(data_table2)[3] <- 3
+#   data_table2$BIG5_Openness <- as.numeric(data_table2$BIG5_Openness)
+#   data_table2$BIG5_Extraversion <- as.numeric(data_table2$BIG5_Extraversion)
+#   data_table2$BIG5_Agreeableness <- as.numeric(data_table2$BIG5_Agreeableness)
+#   data_table2$BIG5_Neuroticism <- as.numeric(data_table2$BIG5_Neuroticism)
+#   data_table2$BIG5_Conscientiousness <- as.numeric(data_table2$BIG5_Conscientiousness)
+#   colnames(data_table2)[1:5] <- sub("BIG5_","",colnames(data_table2))
+#   return(data_table2)
+# }
 
-dm <- function(ams_data_frame1) {
-  max <- rep(1, length(1:5))
-  min <- rep(0, length(1:5))
-  colnames(ams_data_frame1)[7] <- "age"
-  colnames(ams_data_frame1)[9] <- "gender"
-  colnames(ams_data_frame1)[8] <- "age_num"
-  colnames(ams_data_frame1)[10] <- "gender_prob"
-  value <- select(ams_data_frame1, contains("value")) %>%
-  t()
-  rownames(value) <- c()
-  colnames(value) <- c("value")
-  
-  traits <- select(ams_data_frame1, contains("trait")) %>%
-    t() 
-  rownames(traits) <- c()
-  colnames(traits) <- c("trait")
-  data_table <- cbind(traits, value)
-  data_table2 <- data.frame(matrix(seq(5),nrow = 1, ncol = 5))
-  data_table2 <- rbind(data_table2, max, min, data_table[,2])
-  colnames(data_table2) <- data_table[,1]
-  data_table2 <- data_table2[-c(1),]
-  rownames(data_table2)[1] <- 1
-  rownames(data_table2)[2] <- 2
-  rownames(data_table2)[3] <- 3
-  data_table2$BIG5_Openness <- as.numeric(data_table2$BIG5_Openness)
-  data_table2$BIG5_Extraversion <- as.numeric(data_table2$BIG5_Extraversion)
-  data_table2$BIG5_Agreeableness <- as.numeric(data_table2$BIG5_Agreeableness)
-  data_table2$BIG5_Neuroticism <- as.numeric(data_table2$BIG5_Neuroticism)
-  data_table2$BIG5_Conscientiousness <- as.numeric(data_table2$BIG5_Conscientiousness)
-  colnames(data_table2)[1:5] <- sub("BIG5_","",colnames(data_table2))
-  return(data_table2)
+Big5 <- function(predictions) {
+  df <- data.frame()
+  ###add values###
 }
-
-Big5
-
+test <- Big5(predictions1)
