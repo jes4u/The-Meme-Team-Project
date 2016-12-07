621abb5f7b9f0c8b8768aaa7e1ec1b057b773e1c
jes4u <jes4u@uw.edu>
2016-12-07 11:11:49 -0800

    Fixed same vars

--- ui.R
+++ ui.R
@@ -4,7 +4,6 @@ library(knitr)
 
 # Define UI for application
 shinyUI(fluidPage(theme = "bootstrap.css", 
-########################################### new stuff  
        (navbarPage('The Meme Team - Compatibility Test',
                    
                    hr(),
@@ -29,19 +28,15 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                                  
                                ),
                                
-                               ## Not sure if two things work for main panel
                                mainPanel(
                                  plotOutput("word_cloud_1")
                                  
                                  ,
                                  hr()
                                  ,
-                                 ########################## Doesn't show two charts in the mainpanel?
 
                                  plotOutput("word_cloud_2")
                                  
-                                 # ,
-                                 # Insert spider map, keep comma from above
                                  
                                )
                             
@@ -58,32 +53,27 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                               
                               sidebarPanel(
                                 
-                                textInput("t_handle_1", 
+                                textInput("t_handle_3", 
                                           label = h3("Input a Twitter Handle"), 
                                           value = '@realDonaldTrump')
                                 
                                 ,
                                 
-                                textInput("t_handle_2", 
+                                textInput("t_handle_3", 
                                           label = h3("Input a Second Twitter Handle"), 
                                           value = '@HillaryClinton')
                                 
                               ),
                               
-                              ## Not sure if two things work for main panel
                               mainPanel(
-                                plotOutput("word_cloud_1")
                                 
+                                # Insert spider map
+
                                 # ,
-                                # Insert spider map, keep comma from above
-                                
-                                ,
-                                hr()
-                                ,
-                                ########################## Doesn't show two charts in the mainpanel?
-                                
-                                plotOutput("word_cloud_2")
-                                
+                                # hr()
+                                # ,
+
+
                                 # ,
                                 # Insert spider map, keep comma from above
                                 
@@ -98,21 +88,9 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                                
                                
                               
-))
-                  
-                  
-########################################### newstuff                 
+))          
   )
 )
 
 
-########################## old stuff
-  # # Application title
-  # titlePanel("The Meme Team - Compatibility Test"),
-  #   hr(),
-  #   # Show a plot of the generated distribution
-  #   mainPanel("Enter twitter usernames separated by commas.",
-  #     tags$textarea(id="users", rows=3, cols=40, 
-  #                   "samsturtevant, zuzr"),
-  #     textOutput("users")
-  #   )
+
