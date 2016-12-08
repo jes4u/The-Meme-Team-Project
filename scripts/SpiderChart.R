9b1ab13be7d37ff876b100419e75d7a3f01f6bfe
joshuaji03 <yixuaj@uw.edu>
2016-12-07 23:30:10 -0800

    Update SpiderChart.R

--- scripts/SpiderChart.R
+++ scripts/SpiderChart.R
@@ -1,11 +1,10 @@
 library(fmsb)
-data <- c
 # this is the raw spider chart
-radarchart(data)
-  
+  spiderChart <- function(data) {
+    
 colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
 colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
-radarchart( data  , axistype=1 , 
+spider <- radarchart( data  , axistype=1 , 
             #custom polygon
             pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
             #custom the grid
@@ -14,5 +13,7 @@ radarchart( data  , axistype=1 ,
             vlcex=0.8 
 )
 person <- c("person1", "person2")
-legend(x=0.9, y=1, legend = person, bty = "n", pch=20 , 
+bar <- legend(x=0.9, y=1, legend = person, bty = "n", pch=20 , 
        col=colors_in , text.col = "grey", cex=1.2, pt.cex=3)
+  return(spider)
+}
