769da854e596be77d8926a5152bf1c7821ffaef4
joshuaji03 <yixuaj@uw.edu>
2016-12-07 23:14:30 -0800

    Update SpiderChart.R

--- scripts/SpiderChart.R
+++ scripts/SpiderChart.R
@@ -1,40 +1,18 @@
 library(fmsb)
-
+data <- c
 # this is the raw spider chart
 radarchart(data)
-
-# pass in data frame as data. This code returns a spider chart of one person.
-
-
-
-
-# pass in data frame to data. This code returns a spider chart that compares two people.
-
-spiderChart <- function(data) {
-  radarchart( data, axistype=1, 
-              
-              #custom polygon
-              pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
-              
-              #custom the grid
-              cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,1,0.2), cglwd=0.8,
-              
-              #custom labels
-              vlcex=0.8, 
-              colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) ),
-              colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) ),
-              spider <- radarchart( data , axistype=1 , 
-                                    #custom polygon
-                                    pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
-                                    #custom the grid
-                                    cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,1,0.2), cglwd=0.8,
-                                    #custom labels
-                                    vlcex=0.8 
-              )
-  )
-  person <- c("person1", "person2")
-  legend(x=1, y=1, legend = person, bty = "n",
-         pch=20 , col=colors_in , text.col = "grey", cex=1.2, pt.cex=3)
   
-  return(spider)
-}
+colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
+colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
+radarchart( data  , axistype=1 , 
+            #custom polygon
+            pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,1,0.2), cglwd=0.8,
+            #custom labels
+            vlcex=0.8 
+)
+person <- c("person1", "person2")
+legend(x=0.9, y=1, legend = person, bty = "n", pch=20 , 
+       col=colors_in , text.col = "grey", cex=1.2, pt.cex=3)
