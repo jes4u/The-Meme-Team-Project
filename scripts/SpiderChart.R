b6c718c6c4abf45143f53adf9c66f5c8ff91b51d
Ryan Zuzelski <rzuzelski@gmail.com>
2016-12-08 01:17:56 -0800

    same page

--- scripts/SpiderChart.R
+++ scripts/SpiderChart.R
@@ -4,16 +4,13 @@ spiderChart <- function(data, user1, user2) {
     
   colors_border <- c(rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9))
   colors_in <- c(rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4))
-  spider <- radarchart( data, axistype=1, 
+  spider <- radarchart(data, axistype=1, 
             #custom polygon
-            pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+            pcol=colors_border, pfcol=colors_in,
             #custom the grid
-            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,1,0.2), cglwd=0.8,
-            #custom labels
-            vlcex=0.8 
-)
-person <- c(user1, user2)
-bar <- legend(x=0.9, y=1, legend = person, bty = "n", pch=20, 
+            cglcol="grey", axislabcol="grey", caxislabels=seq(0,1,0.2))
+  person <- c(user1, user2)
+  bar <- legend(x=0.9, y=1, legend = person, bty = "n", pch=20, 
        col=colors_in , text.col = "grey", cex=1.2, pt.cex=3)
   return(spider)
 }
